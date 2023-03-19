fe01d00fab3df6130f1bebb8f07a72127c4c4216
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-20 03:01:40 +0500

    correct mongo.uri

--- dist/database/runDB.js
+++ dist/database/runDB.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,16 +31,16 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.runDb = exports.client = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
+const dotenv = __importStar(require("dotenv"));
 const mongodb_1 = require("mongodb");
-dotenv_1.default.config();
-const mongoURI = process.env.MONGO_URI || "mongodb://0.0.0.0:27017";
-console.log(process.env.MONGO_URI);
+dotenv.config();
+const mongoURI = process.env.MONGO_URL;
+if (!mongoURI) {
+    throw Error('Don-don');
+}
+console.log(process.env.MONGO_URL);
 exports.client = new mongodb_1.MongoClient(mongoURI);
 function runDb() {
     return __awaiter(this, void 0, void 0, function* () {
--- src/database/runDB.ts
+++ src/database/runDB.ts
@@ -1,9 +1,12 @@
-import dotenv from 'dotenv'
+import * as dotenv from 'dotenv'
 import {MongoClient} from "mongodb";
 dotenv.config()
 
-const mongoURI = process.env.MONGO_URI || "mongodb://0.0.0.0:27017";
-console.log(process.env.MONGO_URI)
+const mongoURI = process.env.MONGO_URL;
+if(!mongoURI) {
+    throw Error ('Don-don')
+}
+console.log(process.env.MONGO_URL)
 
 export const client = new MongoClient(mongoURI)
 export async function runDb() {
