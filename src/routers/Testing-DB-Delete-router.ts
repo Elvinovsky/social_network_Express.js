230421225b117a50e7eff812cebe8ad9f98f81da
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-20 03:18:51 +0500

    fix

--- src/routers/Testing-DB-Delete-router.ts
+++ src/routers/Testing-DB-Delete-router.ts
@@ -3,6 +3,7 @@ import {blogsRepository} from "../repositories/in-memory/blogs-inMemory-reposito
 import {postsRepository} from "../repositories/in-memory/posts-inMemory-repository";
 
 export const deleteAllDataRouter = Router();
+
 deleteAllDataRouter.delete('/all-data', async (req: Request, res: Response) => {
     await blogsRepository.testingDeleteAllBlogs()
     await postsRepository.testingDeleteAllPosts()
