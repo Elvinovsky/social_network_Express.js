cdc1557cb8f1057e7315116ff9e00451cf4051ce
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-20 03:27:42 +0500

    fix

--- src/middlewares/check-for-errors.ts
+++ src/middlewares/check-for-errors.ts
@@ -4,6 +4,7 @@ const errorFormatter = ({msg}: ValidationError) => {
         // Build your resulting errors however you want! String, object, whatever - it works!
         return {...msg};
     };
+
 export const checkForErrors = ((req: Request, res: Response, next: NextFunction) => {
     const error = validationResult(req).formatWith(errorFormatter);
     if (!error.isEmpty()) {
