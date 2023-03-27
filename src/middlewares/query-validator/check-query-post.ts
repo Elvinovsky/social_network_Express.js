2b0ed9e18e0cab359ea66616c4226518db1e1f5e
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-28 01:49:57 +0500

    delete validationQuery folder

--- src/middlewares/query-validator/check-query-post.ts
+++
@@ -1,8 +0,0 @@
-import {query} from "express-validator";
-
-export const checkQueryTitle =  query('title', )
-    .isLength({ max: 30 })
-    .withMessage( "length should be no more than 15 characters")
-    .bail()
-    .isString()
-    .withMessage("is not a string")
