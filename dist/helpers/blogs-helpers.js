533aee05c3e79be8fd4755d8c699e1761d83d176
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-29 05:42:04 +0500

    fix filters.ts

--- dist/helpers/blogs-helpers.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.blockMongo_Id = void 0;
-exports.blockMongo_Id = { projection: { _id: 0 } };
--- dist/middlewares/query-validator/check-queryBlog.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-/*import {query} from "express-validator";
-
-const checkSearchNameTerm=  query('searchNameTerm', ).default(null)*/ 
