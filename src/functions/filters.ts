4cb326d9a04ca638ff0f8d421a3ebb5533e017ee
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-03-30 16:19:32 +0500

    fix startApp()

--- src/functions/filters.ts
+++ src/functions/filters.ts
@@ -1,12 +1,14 @@
+export type FilterName = {name?: { $regex: string; $options: string}}
+export type FilterTitle = {title?: { $regex: string; $options: string}}
 
-export const filterTitle = (searchTitleTerm: string | null) => {
+export const filterTitle = (searchTitleTerm: string | null): FilterTitle => {
     return  searchTitleTerm
             ? {title : {$regex: searchTitleTerm, $options: 'i'}}
-            : {}
+            : {};
 }
 
-export const filterName = (searchNameTerm: string | null) => {
+export const filterName = (searchNameTerm: string | null): FilterName => {
     return  searchNameTerm
             ? {name : {$regex: searchNameTerm, $options: 'i'}}
-            : {}
+            : {};
 }
