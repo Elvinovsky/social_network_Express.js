d8b6789abdd2e19a868725b59a922f7a29f6cfac
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-04-02 22:57:11 +0500

    fix usersRouter.get

--- dist/routers/users-router.js
+++ dist/routers/users-router.js
@@ -18,9 +18,7 @@ const guard_authentication_1 = require("../middlewares/guard-authentication");
 exports.usersRouter = (0, express_1.Router)();
 exports.usersRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const getAllUsers = yield users_query_repository_1.usersQueryRepository.returnOfAllUsers(req.query.searchEmailTerm, req.query.searchLoginTerm, req.query.pageNumber, req.query.pageSize, req.query.sortBy, req.query.sortDirection);
-    return getAllUsers === null
-        ? res.status(404).send("Users not found")
-        : res.send(getAllUsers);
+    res.send(getAllUsers);
 }));
 exports.usersRouter.post('/', check_bodyUser_1.validatorInputUserBody, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const newUser = yield users_service_1.usersService.createUser(req.body.login, req.body.password, req.body.email);
