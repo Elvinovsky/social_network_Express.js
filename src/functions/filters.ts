3ce745495bdfb98e37da89944e96d54e16c94816
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-04-03 05:01:32 +0500

    FIIIIX

--- src/functions/filters.ts
+++ src/functions/filters.ts
@@ -1,20 +1,8 @@
 
 
-export const filterTitle = (searchTitleTerm?: string ) => {
-    return  searchTitleTerm? {title : {$regex: searchTitleTerm, $options: 'i'}}
-            : {};
-}
-
-export const filterName = {}
-
-
 export const filterLoginOrEmail = (searchEmailTerm?: string, searchLoginTerm?: string ) => {
-    return  searchLoginTerm && searchEmailTerm
-        ? {$and: [{login : {$regex: searchLoginTerm, $options: 'i'}}, {email : {$regex: searchEmailTerm, $options: 'i'}}]}
-        : searchLoginTerm?
-            {login : {$regex: searchLoginTerm, $options: 'i'}}
-            : searchEmailTerm?
-                {email : {$regex: searchEmailTerm, $options: 'i'}}
-                : {}
+    return  searchLoginTerm || searchEmailTerm
+        ? {$or: [{login : {$regex: searchLoginTerm, $options: 'i'}}, {email : {$regex: searchEmailTerm, $options: 'i'}}]}
+        : {}
 }
 export const blockMongo_Id =  {projection:{ _id: 0 }}
