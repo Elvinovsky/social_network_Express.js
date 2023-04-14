5eec49e127acb3553782ddcea9ffb12aab48ef4e
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-04-14 17:31:56 +0500

    fix environment

--- src/settings.ts
+++ src/settings.ts
@@ -1,6 +1,3 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 export const settings = {//todo refactoring?
     MONGO_URI: process.env.MONGO_URL,
     JWT_SECRET: process.env.JWT_SECRET_KEY|| '123',
