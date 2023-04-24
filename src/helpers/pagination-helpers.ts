dc0ef9654b437d0ce98034a0e9ea829e5d455281
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-04-24 23:07:17 +0500

    refactoring pagination-helpers.ts

--- src/helpers/pagination-helpers.ts
+++ src/helpers/pagination-helpers.ts
@@ -12,29 +12,21 @@ export enum SortDirection {
 export const DEFAULT_PAGE_SortBy = 'createdAt'
 const DEFAULT_PAGE_NUMBER = 1
 const DEFAULT_PAGE_SIZE = 10
-
-
-
 export const getPageNumber = (pageNumber?: number ): number => {
-    return pageNumber ? +pageNumber : DEFAULT_PAGE_NUMBER;
-};
-
+    return pageNumber ? +pageNumber : DEFAULT_PAGE_NUMBER
+}
 export const getPageSize = (pageSize?: number): number => {
-    return pageSize ? +pageSize : DEFAULT_PAGE_SIZE;
-};
-
+    return pageSize ? +pageSize : DEFAULT_PAGE_SIZE
+}
 export const getSortBy = (sortBy?: string): string => {
-    return sortBy ? sortBy : DEFAULT_PAGE_SortBy;
-};
-
+    return sortBy ? sortBy : DEFAULT_PAGE_SortBy
+}
 export const getDirection = (sortDirection?: string ) => {
     return sortDirection === 'asc'? SortDirection.Asc : SortDirection.Desc
-};
-
+}
 export const getSkip = (pageNumber: number = 1, pageSize: number = 10): number => {
     return (+pageNumber - 1) * +pageSize;
-};
-
+}
 export const pagesCountOfBlogs  = (calculateOfFiles: number, pageSize?: number): number => {
     return Math.ceil(calculateOfFiles / getPageSize(pageSize))
 }
