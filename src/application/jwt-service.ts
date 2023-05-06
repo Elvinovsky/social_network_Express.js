861df2f702efa07ec2eeedd4ce072c99e49c0a26
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-05-06 19:48:54 +0500

    delete dist

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -15,6 +15,7 @@ export const jwtService = {
     },
     async createJWTRefreshToken ( userId: ObjectId, deviceId: string ): Promise<string> {
 
+
         return  jwt.sign({ userId: userId, deviceId },
             settings.REFRESH_TOKEN_SECRET,
             { expiresIn: '20s' })
