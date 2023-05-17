765689147ed47a508395d7e4317d6be6aa123fa5
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-05-17 20:34:10 +0500

    fix

--- src/middlewares/check-for-errors.ts
+++ src/middlewares/check-for-errors.ts
@@ -14,13 +14,9 @@ export const checkForErrors = ((req: Request, res: Response, next: NextFunction)
 })
 
 export const checkForErrorsSendEmail = ((req: Request, res: Response, next: NextFunction) => {
-    const errorFormatter = ({ msg, param}: ValidationError) => {
-        // Build your resulting errors however you want! String, object, whatever - it works!
-        return {message: msg, field: param};
-    };
-    const error = validationResult(req).formatWith(errorFormatter);
-    if (!error.isEmpty()) {
-        return res.status(204)
+    const invalid = validationResult(req)
+    if (!invalid.isEmpty()) {
+        return res.sendStatus(204)
     }
     return next()
 })
