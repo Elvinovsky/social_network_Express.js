e66893c8fb21cc9672ee41b04147fffaae9a1b4d
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-05-28 23:37:41 +0500

    corrected blogs e2e tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "@shelf/jest-mongodb",
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 100000, //от этой ошибки! -> thrown: "Exceeded timeout of 5000 ms for a test.
   testRegex: '.e2e.test.ts$', //<-- чтобы запускались только файлы с расширением ".e2e.test.ts"
