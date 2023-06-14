8ffc22546c56d21aa9a903d915ecee636a66eb10
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-06-14 23:37:12 +0500

    ref

--- src/functions/commentsMapping.ts
+++ src/functions/commentsMapping.ts
@@ -1,9 +1,10 @@
-import {WithId} from "mongodb";
-import {CommentViewModel} from "../models/modelsComment/comment-view";
-import {CommentDBModel} from "../models/modelsComment/comment-input";
+import { WithId } from "mongodb";
+import { CommentViewModel } from "../models/modelsComment/comment-view";
+import { CommentDBModel } from "../models/modelsComment/comment-input";
 
-export const commentsMapping = (array: Array<WithId<CommentDBModel>>): CommentViewModel[] =>{
-    return array.map((el) => {
+export const commentsMapping = ( array: Array<WithId<CommentDBModel>> ): CommentViewModel[] => {
+    return array.map(( el ) => {
+
         return {
             id: el._id.toString(),
             content: el.content,
@@ -20,7 +21,7 @@ export const commentsMapping = (array: Array<WithId<CommentDBModel>>): CommentVi
         }
     })
 }
-export const commentMapping = (comment: WithId<CommentDBModel> ): CommentViewModel => {
+export const commentMapping = ( comment: WithId<CommentDBModel> ): CommentViewModel => {
     return {
         id: comment._id.toString(),
         content: comment.content,
