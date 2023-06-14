ffadb6ef528d59a16420498384d0a3ab9c3ca42d
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-06-14 14:47:55 +0500

    refactoring

--- src/middlewares/body-validator/check-bodyBlog.ts
+++ src/middlewares/body-validator/check-bodyBlog.ts
@@ -1,35 +1,38 @@
-import { body} from 'express-validator'
-import {checkForErrors} from "../check-for-errors";
-import {superAdminAuthentication} from "../guard-authentication/super-admin-authentication";
+import { body } from 'express-validator'
+import { checkForErrors } from "../check-for-errors";
+import { superAdminAuthentication } from "../guard-authentication/super-admin-authentication";
 
-const checkInputName =  body('name', )
+const checkInputName = body('name',)
     .trim()
-    .isLength({min: 3, max: 15 })
-    .withMessage( "length should be no more than 15 characters")
+    .isLength({
+        min: 3,
+        max: 15
+    })
+    .withMessage("length should be no more than 15 characters")
     .bail()
     .isString()
     .withMessage("is not a string")
-const checkInputWebsiteUrl =  body('websiteUrl')
-    .matches( /^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)
+const checkInputWebsiteUrl = body('websiteUrl')
+    .matches(/^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)
     .withMessage("is not a link to the site")
     .bail()
     .isString()
     .withMessage("is not a string")
-    .isLength({min:10, max: 100 })
+    .isLength({
+        min: 10,
+        max: 100
+    })
     .withMessage("must be at least 100 chars long")
-const checkInputDescription =  body ( 'description')
+const checkInputDescription = body('description')
     .trim()
-    .isLength({ min: 3, max: 500 })
+    .isLength({
+        min: 3,
+        max: 500
+    })
     .withMessage("length should be no more than 500 characters")
     .bail()
     .isString()
     .withMessage("is not a string")
 
 
-export const validatorBlogInputBody = [
-        superAdminAuthentication,
-        checkInputName,
-        checkInputWebsiteUrl,
-        checkInputDescription,
-        checkForErrors
-    ]
+export const validatorBlogInputBody = [superAdminAuthentication, checkInputName, checkInputWebsiteUrl, checkInputDescription, checkForErrors]
