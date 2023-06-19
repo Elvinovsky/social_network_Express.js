a1a9894d756c7b654048242fcbf32883b39eeea5
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-06-19 22:43:08 +0500

    refactoring

--- src/helpers/email-helpers.ts
+++ src/helpers/email-helpers.ts
@@ -3,29 +3,31 @@ import {
 } from 'nodemailer'
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-export const transporterVerify = ( transporter:  Transporter<SMTPTransport.SentMessageInfo>) => {
-    return new Promise (( resolve, reject) => {
-        transporter.verify(function(error, success){
-            if(error) {
+export const transporterVerify = ( transporter: Transporter<SMTPTransport.SentMessageInfo> ) => {
+    return new Promise(( resolve, reject ) => {
+        transporter.verify(function ( error, success ) {
+            if (error) {
                 console.log(error)
                 reject(error)
-            } else {
-                console.log( 'Server is ready to take our messages')
+            } else{
+                console.log('Server is ready to take our messages')
                 resolve(success)
             }
         })
     })
 }
-export function sendMailPromise(transporter:  Transporter<SMTPTransport.SentMessageInfo>, mailOptions: {}) {
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                console.log(info);
-                resolve(info);
-            }
-        });
+
+export function sendMailPromise ( transporter: Transporter<SMTPTransport.SentMessageInfo>, mailOptions: {} ) {
+    return new Promise(( resolve, reject ) => {
+        transporter.sendMail(mailOptions,
+            ( err, info ) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else{
+                    console.log(info);
+                    resolve(info);
+                }
+            });
     });
 }
