a82f4caf53d87ce0c61115d2033baceedb6742fd
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-12 22:02:31 +0500

    Update posts-controller.ts  "added createPost, createCommentByPost, getCommentsByPostId methods"

--- src/controllers/posts-controller.ts
+++ src/controllers/posts-controller.ts
@@ -1,5 +1,8 @@
 import { injectable } from "inversify";
 import {
+    RequestInputBody,
+    RequestParamsAndInputBody,
+    RequestParamsAndInputQuery,
     RequestQuery,
     ResponseViewBody
 } from "../types/req-res-types";
@@ -10,6 +13,13 @@ import {
 import { PaginatorType } from "../helpers/pagination-helpers";
 import { PostView } from "../models/modelsPosts/post-view";
 import { postQueryRepository } from "../repositories/queryRepository/posts-query-repository";
+import { CommentViewModel } from "../models/modelsComment/comment-view";
+import { CommentInputModel } from "../models/modelsComment/comment-input";
+import {
+    feedbacksService,
+    postsService
+} from "../compositions-root";
+import { PostInput } from "../models/modelsPosts/post-input";
 
 @injectable()
 export class PostsController {
@@ -22,4 +32,50 @@ export class PostsController {
             req.user?.id)
         res.send(getAllPosts)
     }
+
+    async getCommentsByPostId ( req: RequestParamsAndInputQuery<{
+        postId: string
+    }, QueryInputParams>, res: ResponseViewBody<PaginatorType<CommentViewModel[]>> ) {
+
+        const getCommentsByPostId = await postQueryRepository.getCommentsByPostId(req.params.postId,
+            Number(req.query.pageNumber),
+            Number(req.query.pageSize),
+            req.query.sortBy,
+            req.query.sortDirection,
+            req.user?.id)
+
+        if (!getCommentsByPostId) {
+            res.sendStatus(404)
+            return;
+        }
+        res.send(getCommentsByPostId)
+        return
+    }
+
+    async createCommentByPost ( req: RequestParamsAndInputBody<{
+        postId: string
+    }, CommentInputModel>, res: ResponseViewBody<CommentViewModel> ) {
+
+        const validatorPostIdForCreateComments = await feedbacksService.findPostIdForComments(req.params.postId)
+        if (!validatorPostIdForCreateComments) {
+            res.sendStatus(404)
+            return;
+        }
+        const comment = await feedbacksService.createComment(req.params.postId,
+            req.user!.id,
+            req.body.content)
+        res.status(201)
+           .send(comment)
+    }
+    async createPost( req: RequestInputBody<PostInput>, res: ResponseViewBody<PostView> ) {
+
+    const createdNewPost = await postsService.createPost(req.body.title,
+        req.body.shortDescription,
+        req.body.content,
+        req.body.blogId)
+
+    res.status(201)
+.send(createdNewPost)
+    return;
+}
 }
