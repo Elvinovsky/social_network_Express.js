891b72bbef51f2c298648f54fb4eee9c4f2c597b
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-07-29 21:55:41 +0500

    Update auths-controller.ts  "renaming"

--- src/controllers/auths-controller.ts
+++ src/controllers/auths-controller.ts
@@ -26,122 +26,151 @@ import { AuthService } from "../domains/auth-service";
 
 @injectable()
 export class AuthController {
-    constructor ( @inject(JwtService) protected jwtService: JwtService,
-                  @inject(DevicesService) protected devicesService: DevicesService,
-                  @inject(DevicesSessionsRepository) protected devicesSessionsRepository: DevicesSessionsRepository,
-                  @inject(AuthService) protected authService: AuthService) { }
+    constructor(
+        @inject(JwtService) protected jwtService: JwtService,
+        @inject(DevicesService) protected devicesService: DevicesService,
+        @inject(DevicesSessionsRepository) protected devicesSessionsRepository: DevicesSessionsRepository,
+        @inject(AuthService) protected authService: AuthService
+    ) {}
 
-    async createLogin ( req: Request, res: Response ) {
-        const user = await this.authService.checkCredentials(req.body.loginOrEmail,
-            req.body.password)
-        if (!user) return res.sendStatus(401)
+    async createLogin(req: Request, res: Response) {
+        const user = await this.authService.checkCredentials(
+            req.body.loginOrEmail,
+            req.body.password
+        );
+        if (!user) {
+            return res.sendStatus(401);
+        }
 
-        const deviceId = uuidv4()
-        const accessToken = await this.jwtService.createJWTAccessToken(user._id)
-        const refreshToken = await this.jwtService.createJWTRefreshToken(user._id,
-            deviceId)
+        const deviceId = uuidv4();
+        const accessToken = await this.jwtService.createJWTAccessToken(user._id);
+        const refreshToken = await this.jwtService.createJWTRefreshToken(
+            user._id,
+            deviceId
+        );
 
-        const ipAddress = requestIp.getClientIp(req)
-        const deviceName = req.headers["user-agent"]
-        const issuedAt = await this.jwtService.getIATByRefreshToken(refreshToken)
-        await this.devicesService.createDeviceSession(user._id,
+        const ipAddress = requestIp.getClientIp(req);
+        const deviceName = req.headers["user-agent"];
+        const issuedAt = await this.jwtService.getIATByRefreshToken(refreshToken);
+        await this.devicesService.createDeviceSession(
+            user._id,
             deviceId,
             issuedAt!,
             ipAddress,
-            deviceName,)
+            deviceName
+        );
 
         return res
             .status(200)
-            .cookie('refreshToken',
-                refreshToken,
-                refreshCookieOptions)
-            .send(accessToken)
+            .cookie("refreshToken", refreshToken, refreshCookieOptions)
+            .send(accessToken);
     }
 
-    async createRefToken ( req: Request, res: Response ) {
-        const accessToken = await this.jwtService.createJWTAccessToken(req.userId)
-        const newRefreshToken = await this.jwtService.createJWTRefreshToken(req.userId,
-            req.deviceId)
+    async createRefToken(req: Request, res: Response) {
+        const accessToken = await this.jwtService.createJWTAccessToken(req.userId);
+        const newRefreshToken = await this.jwtService.createJWTRefreshToken(
+            req.userId,
+            req.deviceId
+        );
 
-        const newIssuedAt = await this.jwtService.getIATByRefreshToken(newRefreshToken)
-        await this.devicesService.updateIATByDeviceSession(newIssuedAt!,
-            req.issuedAt)
+        const newIssuedAt = await this.jwtService.getIATByRefreshToken(
+            newRefreshToken
+        );
+        await this.devicesService.updateIATByDeviceSession(
+            newIssuedAt!,
+            req.issuedAt
+        );
 
-        return res.status(200)
-                  .cookie('refreshToken',
-                      newRefreshToken,
-                      refreshCookieOptions)
-                  .send(accessToken)
+        return res
+            .status(200)
+            .cookie("refreshToken", newRefreshToken, refreshCookieOptions)
+            .send(accessToken);
     }
 
-    async logout ( req: Request, res: Response ) {
-        await this.devicesSessionsRepository.deleteDeviceSessionByIAT(req.issuedAt)
-        return res.clearCookie('refreshToken')
-                  .sendStatus(204)
+    async logout(req: Request, res: Response) {
+        await this.devicesSessionsRepository.deleteDeviceSessionByIAT(
+            req.issuedAt
+        );
+        return res.clearCookie("refreshToken").sendStatus(204);
     }
 
-    async registration ( req: Request, res: Response ) {
-        const user = await this.authService.userRegistration(req.body.login,
+    async registration(req: Request, res: Response) {
+        const user = await this.authService.userRegistration(
+            req.body.login,
             req.body.password,
-            req.body.email)
+            req.body.email
+        );
         if (user) {
-            res.sendStatus(204)
-            return
+            res.sendStatus(204);
+            return;
         }
 
-        res.sendStatus(400)
+        res.sendStatus(400);
     }
 
-    async registrationConfirm ( req: RequestInputBody<RegistrationConfirmationCodeModel>, res: Response ) {
-        const isConfirmed = await this.authService.confirmCode(req.body.code)
+    async registrationConfirm(
+        req: RequestInputBody<RegistrationConfirmationCodeModel>,
+        res: Response
+    ) {
+        const isConfirmed = await this.authService.confirmCode(req.body.code);
         if (isConfirmed) {
-            res.sendStatus(204)
-            return
+            res.sendStatus(204);
+            return;
         }
-        res.sendStatus(400)
+        res.sendStatus(400);
     }
 
-    async emailResending ( req: RequestInputBody<RegistrationEmailResending>, res: Response ) {
-        const isSentCode = await this.authService.emailResending(req.body.email)
+    async emailResending(
+        req: RequestInputBody<RegistrationEmailResending>,
+        res: Response
+    ) {
+        const isSentCode = await this.authService.emailResending(req.body.email);
         if (isSentCode) {
-            res.sendStatus(204)
-            return
+            res.sendStatus(204);
+            return;
         }
-        res.sendStatus(400)
-        return
+        res.sendStatus(400);
     }
 
-    async passwordRecovery ( req: RequestInputBody<PasswordRecoveryInputModel>, res: Response ) {
-        const isSentCode = await this.authService.sendPasswordRecovery(req.body.email)
+    async passwordRecovery(
+        req: RequestInputBody<PasswordRecoveryInputModel>,
+        res: Response
+    ) {
+        const isSentCode = await this.authService.sendPasswordRecovery(
+            req.body.email
+        );
         if (isSentCode) {
-            res.sendStatus(204)
-            return
+            res.sendStatus(204);
+            return;
         }
-        res.sendStatus(400)
-        return
+        res.sendStatus(400);
     }
 
-    async newPassword ( req: RequestInputBody<NewPasswordRecoveryInputModel>, res: Response ) {
-
-        const recoveryPassword = await this.authService.passwordRecovery(req.body.newPassword,
-            req.body.recoveryCode)
+    async newPassword(
+        req: RequestInputBody<NewPasswordRecoveryInputModel>,
+        res: Response
+    ) {
+        const recoveryPassword = await this.authService.passwordRecovery(
+            req.body.newPassword,
+            req.body.recoveryCode
+        );
         if (recoveryPassword === null) {
-            res.sendStatus(403)
-            return
+            res.sendStatus(403);
+            return;
         }
         if (recoveryPassword) {
-            res.sendStatus(204)
-            return
+            res.sendStatus(204);
+            return;
         }
-        res.sendStatus(400)
+        res.sendStatus(400);
     }
 
-    async getMe ( req: Request, res: Response ) {
-        const user = await usersQueryRepository.getUserInfo(req.user!.id)
+    async getMe(req: Request, res: Response) {
+        const user = await usersQueryRepository.getUserInfo(req.user!.id);
         if (user) {
-            res.send(user)
-            return
+            res.send(user);
+            return;
         }
+        res.sendStatus(404);
     }
 }
-
