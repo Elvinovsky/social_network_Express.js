ca79d3afa4e10f819944b29ca6d7faf0af4eb52d
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-02 19:42:56 +0500

    Update device-input.ts "delete type"

--- src/models/modelsDevice/device-input.ts
+++ src/models/modelsDevice/device-input.ts
@@ -1,21 +1,3 @@
-export type SessionInputModel = {
-    ip:	string
-    /**
-     * IP address of device during signing in
-     */
-    title:	string
-    /**
-     * deviceName: for example Chrome 105 (received by parsing http header "user-agent")
-     */
-    lastActiveDate:	string
-    /**
-     *  Date of the last generating of refresh/access tokens
-     */
-    deviceId:	string
-    /**
-     * id of connected device session
-     */
-}
 export type SessionDBModel = {
     deviceId:	string
     issuedAt: number
