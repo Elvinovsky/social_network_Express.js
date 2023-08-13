4ee2e0a7aeb9fcf70094b5fc6956e367beaea260
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-13 21:17:50 +0500

    Update devices-service.ts 'ref'

--- src/domains/devices-service.ts
+++ src/domains/devices-service.ts
@@ -12,6 +12,15 @@ export class DevicesService {
     constructor ( @inject(DevicesSessionsRepository) protected devicesSessionsRepository: DevicesSessionsRepository ) {
     }
 
+    /**
+     * Создает новую сессию устройства.
+     * @param userId Идентификатор пользователя (ObjectId).
+     * @param deviceId Идентификатор устройства.
+     * @param issuedAt Время создания токена.
+     * @param ip IP-адрес устройства.
+     * @param deviceName Название устройства.
+     * @returns Промис с результатом добавления сессии устройства.
+     */
     async createDeviceSession ( userId: ObjectId, deviceId: string, issuedAt: number, ip: string | null, deviceName?: string ) {
         const createDeviceSession: SessionDBModel = {
             deviceId: deviceId,
@@ -27,14 +36,25 @@ export class DevicesService {
                 })
         }
         return await this.devicesSessionsRepository.addDeviceSession(createDeviceSession)
-
     }
 
+    /**
+     * Обновляет время создания (IAT) в сессии устройства.
+     * @param newIssuedAt Новое время создания токена.
+     * @param issuedAt Старое время создания токена.
+     * @returns Промис с результатом обновления.
+     */
     async updateIATByDeviceSession ( newIssuedAt: number, issuedAt: number ) {
         return await this.devicesSessionsRepository.updateDeviceSession(newIssuedAt,
             issuedAt)
     }
 
+    /**
+     * Выход из сессии устройства по его идентификатору и идентификатору пользователя.
+     * @param deviceId Идентификатор устройства.
+     * @param userId Идентификатор пользователя (строка).
+     * @returns Промис с результатом удаления сессии устройства.
+     */
     async logoutDeviceSessionByDeviceId ( deviceId: string, userId: string ) {
         const findDeviceSessionById = await this.devicesSessionsRepository.findDeviceIdAmongSessions(deviceId)
         if (!findDeviceSessionById) {
@@ -44,6 +64,12 @@ export class DevicesService {
             userId)
     }
 
+    /**
+     * Выход из всех сессий устройств для указанного пользователя.
+     * @param issuedAt Время создания токена.
+     * @param userId Идентификатор пользователя (строка).
+     * @returns Промис с результатом удаления сессий устройств.
+     */
     async logoutDevicesSessionsByUser ( issuedAt: number, userId: string ) {
         const result = this.devicesSessionsRepository.deleteDevicesSessionsByUser(issuedAt,
             userId)
