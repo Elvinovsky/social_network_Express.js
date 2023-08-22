bd126939aae4f56eb85c9c6f1fcb64c3a762de31
Agakerimov Elvin <shipagakerimov@mail.ru>
2023-08-22 19:20:29 +0500

    Update auth-service.ts 'refactoring'

--- src/domains/auth-service.ts
+++ src/domains/auth-service.ts
@@ -17,8 +17,18 @@ export class AuthService {
     constructor ( @inject(EmailsManager) protected emailsManager: EmailsManager ) {
     }
 
+    /**
+     * Регистрация нового пользователя.
+     * @param login Логин нового пользователя.
+     * @param password Пароль нового пользователя.
+     * @param email Адрес электронной почты нового пользователя.
+     * @returns Объект UserViewModel, представляющий зарегистрированного пользователя, или null в случае ошибки.
+     */
     async userRegistration ( login: string, password: string, email: string ): Promise<UserViewModel | null> {
-        const hash = await this._generateHash(password)
+        // Генерация хеша пароля
+        const hash = await this._generateHash(password);
+
+        // Создание нового пользователя с подготовленными данными
         const newUser: UserDBType = {
             login: login,
             passwordHash: hash,
@@ -33,103 +43,163 @@ export class AuthService {
                     }),
                 isConfirmed: false
             }
-        }
-        const result = await usersRepository.addNewUser(newUser)
+        };
+
+        // Добавление нового пользователя в базу данных
+        const result = await usersRepository.addNewUser(newUser);
 
         try {
+            // Отправка сообщения для подтверждения адреса электронной почты
             await emailsManager.sendEmailConformationMessage(email,
-                newUser.emailConfirmation.confirmationCode)
+                newUser.emailConfirmation.confirmationCode);
         } catch (error) {
-            console.error(error)
-            await usersRepository.userByIdDelete(result.id)
-            return null
+            console.error(error);
+
+            // В случае ошибки удаление пользователя из базы данных
+            await usersRepository.userByIdDelete(result.id);
+            return null;
         }
-        return result
+
+        return result;
     }
 
+    /**
+     * Восстановление пароля пользователя.
+     * @param password Новый пароль для пользователя.
+     * @param code Код подтверждения восстановления пароля.
+     * @returns true, если восстановление пароля успешно, false в случае ошибки.
+     */
     async passwordRecovery ( password: string, code: string ): Promise<boolean | null> {
-        const isConfirmed = await this.confirmCode(code)
-        const isOneUser = await usersRepository.getUsersByConfirmationCode(code)
+        // Проверка подтверждения кода восстановления
+        const isConfirmed = await this.confirmCode(code);
+        // Проверка наличия пользователей с данным кодом
+        const isOneUser = await usersRepository.getUsersByConfirmationCode(code);
+
         if (!isConfirmed && isOneUser) {
-            return null
+            return null;
         }
 
-        const hash = await this._generateHash(password)
+        // Генерация хеша нового пароля
+        const hash = await this._generateHash(password);
+
+        // Обновление хеша пароля в базе данных
         return usersRepository.updatePasswordHash(hash,
-            code)
+            code);
     }
 
+    /**
+     * Подтверждение кода подтверждения электронной почты пользователя.
+     * @param code Код подтверждения электронной почты.
+     * @returns true, если код успешно подтвержден, false в случае ошибки.
+     */
     async confirmCode ( code: string ): Promise<boolean> {
-
-        return await usersRepository.updateConfirmCode(code)
-
+        // Обновление кода подтверждения в базе данных
+        return await usersRepository.updateConfirmCode(code);
     }
 
+    /**
+     * Повторная отправка подтверждающего сообщения на электронную почту.
+     * @param email Адрес электронной почты пользователя.
+     * @returns true, если сообщение успешно отправлено, false в случае ошибки.
+     */
     async emailResending ( email: string ): Promise<boolean> {
-        const newCode = uuidv4()
+        // Генерация нового кода подтверждения
+        const newCode = uuidv4();
+
+// Обновление кода подтверждения в базе данных
         const codeReplacement = await usersRepository.updateConfirmationCodeByEmail(email,
-            newCode)
+            newCode);
 
         if (!codeReplacement) {
-            return false
+            return false;
         }
 
         try {
+            // Отправка сообщения для подтверждения адреса электронной почты
             await this.emailsManager.sendEmailConformationMessage(email,
-                newCode)
+                newCode);
         } catch (error) {
-            const user = await usersRepository.findByLoginOrEmail(email)
-            await usersRepository.userByIdDelete(user!._id.toString()) // емайл не подтвержден! user валидириуется в верхних слоях экспресс валидатора
-            console.error(error)
-            return false
+            const user = await usersRepository.findByLoginOrEmail(email);
+            await usersRepository.userByIdDelete(user!._id.toString()); // Email не подтвержден! User валидируется в верхних слоях express-валидатора.
+            console.error(error);
+            return false;
         }
-        return true
+
+        return true;
     }
 
+    /**
+     * Отправка сообщения о восстановлении пароля на электронную почту.
+     * @param email Адрес электронной почты пользователя.
+     * @returns true, если сообщение успешно отправлено, false в случае ошибки.
+     */
     async sendPasswordRecovery ( email: string ): Promise<boolean> {
-        const newCode = uuidv4()
+        // Генерация нового кода подтверждения
+        const newCode = uuidv4();
+
+        // Обновление кода подтверждения в базе данных
         const codeReplacement = await usersRepository.updateConfirmationCodeByEmail(email,
-            newCode)
+            newCode);
 
         if (!codeReplacement) {
-            return false
+            return false;
         }
 
         try {
+            // Отправка сообщения о восстановлении пароля
             await this.emailsManager.sendEmailPasswordRecovery(email,
-                newCode)
+                newCode);
         } catch (error) {
-            const user = await usersRepository.findByLoginOrEmail(email)
-            await usersRepository.userByIdDelete(user!._id.toString()) // емайл подтвержден! user валидириуется при запросе на эндпоинт экспресс валидатором
-            console.error(error)
-            return false
+            const user = await usersRepository.findByLoginOrEmail(email);
+            await usersRepository.userByIdDelete(user!._id.toString()); // Email подтвержден! User валидируется при запросе на эндпоинт express-валидатором.
+            console.error(error);
+            return false;
         }
 
-        return true
+        return true;
     }
 
+    /**
+     * Проверка учетных данных (логин/электронная почта и пароль) пользователя.
+     * @param loginOrEmail Логин или адрес электронной почты пользователя.
+     * @param password Пароль пользователя.
+     * @returns Объект WithId<UserDBType>, представляющий пользователя, если учетные данные верны, или null в случае ошибки.
+     */
     async checkCredentials ( loginOrEmail: string, password: string ): Promise<WithId<UserDBType> | null> {
-        const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
+        // Поиск пользователя по логину или адресу электронной почты
+        const user = await usersRepository.findByLoginOrEmail(loginOrEmail);
         if (!user || !user.emailConfirmation.isConfirmed) {
-            return null
+            return null;
         }
 
+        // Проверка соответствия хеша пароля
         const isHashesEquals = await this._isPasswordCorrect(password,
-            user.passwordHash)
+            user.passwordHash);
         if (isHashesEquals) {
-            return user
+            return user;
         } else{
-            return null
+            return null;
         }
     }
 
+    /**
+     * Генерация хеша пароля.
+     * @param password Пароль, для которого нужно сгенерировать хеш.
+     * @returns Хеш пароля.
+     */
     async _generateHash ( password: string ): Promise<string> {
         return await bcrypt.hash(password,
-            7)
+            7);
     }
 
+    /**
+     * Проверка соответствия пароля и хеша.
+     * @param password Пароль для сравнения.
+     * @param hash Хеш, с которым нужно сравнить пароль.
+     * @returns true, если пароль соответствует хешу, false в противном случае.
+     */
     async _isPasswordCorrect ( password: string, hash: string ): Promise<boolean> {
         return await bcrypt.compare(password,
-            hash)
+            hash);
     }
 }
